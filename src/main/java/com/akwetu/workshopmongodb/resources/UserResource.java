007b5db80e732899dd2284f1527b5076c937150e
José Costa <kosta10alberto@gmail.com>
2023-03-06 02:03:45 +0300

    DTO for to return user

--- src/main/java/com/akwetu/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/akwetu/workshopmongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.akwetu.workshopmongodb.resources;
 
 import com.akwetu.workshopmongodb.Services.UserService;
+import com.akwetu.workshopmongodb.dto.UserDTO;
 import com.akwetu.workshopmongodb.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
@@ -17,9 +19,10 @@ public class UserResource {
     private UserService service;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 
 }
